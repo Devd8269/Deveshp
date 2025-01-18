1092fef4f152f22a1e68bfacd060811fedfaf0ac
deves <deves@Devesh>
2025-01-18 09:54:16 +0530

    added new pom class, product class is added

---
+++ SyleniumTekCrm2/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+@FindBy(xpath = "//input[@alt='Create Product...']")
+private WebElement createproductimgBtn;
+}
